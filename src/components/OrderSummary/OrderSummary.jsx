400b39d648ec44247a5b634f9027f56fe8afa699
Gabor <tgabri@gmail.com>
2019-10-24 12:36:47 +0100

    button component created

--- src/components/OrderSummary/OrderSummary.jsx
+++ src/components/OrderSummary/OrderSummary.jsx
@@ -15,6 +15,8 @@ export default function OrderSummary(props) {
       <p>A delicious burger with the following ingredients:</p>
       <ul>{ingredientSummary}</ul>
       <p>Continue to Checkout?</p>
+      <button>CANCEL</button>
+      <button>CONTINUE</button>
     </React.Fragment>
   );
 }
---
+++ src/components/Reusable/Button.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import classes from './Button.css';
+
+export default function Button(props) {
+  return <button className={[classes.Button, classes.[props.btnType]]} onClick={props.clicked}>{props.children}</button>;
+}
